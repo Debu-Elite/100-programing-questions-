6488495f8859225182d00f3692b0c3317a581e9b
Debu-Elite <gautamdevansh183@gmail.com>
2025-11-14 09:26:56 +0000

    input from fgets

--- c/input_output.c
+++ c/input_output.c
@@ -4,16 +4,14 @@ int main()
 {
 int x=10;
 int y;
+printf("enter the string:");
+char str[50];
+fgets(str, sizeof(str), stdin);
 printf("enter the value for interger\n");
 scanf("%d",&y);
 float f =4.4;
 double d =8.4444;
 char *ptr="hello from c";
-char str[50];
-printf("enter the string:");
-scanf("%s",str);
-
-
 
 printf("x=%d  y=%d\n",x,y);
 printf("flaot value %f\n",f);
