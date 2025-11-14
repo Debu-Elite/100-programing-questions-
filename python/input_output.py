9c1af86fec78a078b320db30e2752f0ba52c598b
Debu-Elite <gautamdevansh183@gmail.com>
2025-11-14 08:57:57 +0000

    all python basic types

--- python/input_output.py
+++ python/input_output.py
@@ -1,6 +1,14 @@
 a=int(input("enter a number"))
 b=input("inter string")
-
-
+c=None
+d=float(input("enter the float value"))
+complex1=5+10j
+complex2=complex(input("enter complex number"))
 print(a)
 print(b)
+print(c)
+print(d)
+print(complex1.real)
+print(complex1.imag)
+print(complex2.real)
+print(complex2.imag)
